5f6c080cae6971b27be1ea41fd7a76e099d45667
Seppe Dekeyser <seppe.dekeyser@hotmail.com>
2025-06-17 01:17:31 +0200

    Nix devshell flake and Houdini version tweak

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs;
+        yarn = (prev.yarn.override { inherit nodejs; });
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nil
+              node2nix
+              nodejs
+              nodePackages.pnpm
+              yarn
+            ];
+          };
+        }
+      );
+    };
+}
